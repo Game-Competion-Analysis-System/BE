ac335ed9e52574e09b5bc742a52704e0b42fbb3a
Your Name <you@example.com>
2026-01-26 22:14:02 +0700

    Initial BE - 3 layer architecture (Controller, BIL, DAL)

---
+++ Controllers/AIController.cs
@@ -0,0 +1,27 @@
+﻿using BIL.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameCompetionAnalysisSystem.Controllers
+{
+    [ApiController]
+    [Route("api/ai")]
+    public class AIController : ControllerBase
+    {
+        private readonly IAIAnalysisService _service;
+
+        public AIController(IAIAnalysisService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("analyze")]
+        [Consumes("multipart/form-data")]
+        public async Task<IActionResult> Analyze(IFormFile file)
+        {
+            var result = await _service.AnalyzeScreenshotAsync(file);
+            return Ok(result);
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+using BIL.Service;
+using DAL.Entities;
+using DAL.Repository;
+using System.Net.Http.Headers;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers()
+    .AddJsonOptions(opt =>
+    {
+        opt.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
+builder.Services.AddHttpClient<IAIAnalysisService, AIAnalysisService>((sp, client) =>
+{
+    var config = sp.GetRequiredService<IConfiguration>();
+    var apiKey = config["Mistral:ApiKey"];
+
+    client.BaseAddress = new Uri("https://api.mistral.ai/");
+    client.DefaultRequestHeaders.Authorization =
+        new AuthenticationHeaderValue("Bearer", apiKey);
+});
+
+builder.Services.AddHttpClient<IAIAnalysisRepository, AIAnalysisRepository>(client =>
+{
+    client.Timeout = TimeSpan.FromMinutes(10);
+});
+builder.Services.AddScoped<ILeaderboardService, LeaderboardService>();
+builder.Services.AddScoped<ILeaderboardRepository, LeaderboardRepository>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<PostgresContext>();
+builder.Services.AddScoped<IAIAnalysisRepository, AIAnalysisRepository>();
+builder.Services.AddScoped<IAIAnalysisService, AIAnalysisService>();
+
+builder.Services.AddScoped<IGameRepository, GameRepository>();
+builder.Services.AddScoped<IGameService, GameService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
