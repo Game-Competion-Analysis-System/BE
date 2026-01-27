057f6be3e372cdb907f3f5b586f3f9b407083683
Your Name <you@example.com>
2026-01-27 08:23:42 +0700

    add folder

---
+++ DAL/Entities/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAL.Entities;
+
+public partial class Game
+{
+    public int Gameid { get; set; }
+
+    public string Gamename { get; set; } = null!;
+
+    public string? Genre { get; set; }
+
+    public int? Companyid { get; set; }
+
+    public virtual Company? Company { get; set; }
+
+    public virtual ICollection<Event> Events { get; set; } = new List<Event>();
+
+    public virtual ICollection<Player> Players { get; set; } = new List<Player>();
+}
